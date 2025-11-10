2a5ceae2d59f656e771b6dd47c444c0f93a81d5f
ran7 <ran.dror@campus.technion.ac.il>
2025-11-10 16:13:16 +0200

    plot commit

--- start.jl
+++ start.jl
@@ -1,6 +1,13 @@
+using Plots
+
+
 x = 4
 y = x+6
 println(y)
 z = 0.5*pi
 println(sin(z))
 
+x = [0.5*pi, pi, 1.5*pi, 2pi]
+y = sin.(x)
+
+plot(x, y, label="sin(x)", xlabel="x", ylabel="sin(x)", title="Plot of sin(x)",mark="o")
