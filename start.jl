4753a4f6f455d016deb4aaa253448a65683d820f
ran7 <ran.dror@campus.technion.ac.il>
2025-11-10 17:07:38 +0200

    adding sine plot

--- start.jl
+++ start.jl
@@ -3,10 +3,20 @@ using PyPlot
 x = 0:0.1*pi:2pi
 y = cos.(x)
 
-plot(x, y, "o", label="cos(x)")   # "o" → dots only
+plot(x, y, "o--", label="cos(x)")   # "o" → dots only
 title("Cosine Function")
 xlabel("x")
 ylabel("cos(x)")
+legend()
+grid(true)
+show()
+
+z = sin.(x)
+#add a plot of sin
+plot(x, z, "o--", label="sin(x)")   # "o" → dots only
+title("Cosine Function")
+xlabel("x")
+ylabel("sin(x)")
 legend()
 grid(true)
 show()
